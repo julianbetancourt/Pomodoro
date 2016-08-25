e52c0858f1d2ac4545c517a9ede5ddd1d4280969
Julian Betancourt <julianbetancourt10@gmail.com>
2016-08-24 22:27:06 -0500

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+//import './index.css';
+import './sass/main.sass';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
